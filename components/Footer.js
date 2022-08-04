bbebaa89a8b0f1ccc64b6f0c2b82964fc2b4db08
hasnatsajid965 <86589885+hasnatsajid@users.noreply.github.com>
2022-08-04 19:04:17 +0500

    minimal menu done

--- components/Footer.js
+++ components/Footer.js
@@ -1,5 +1,5 @@
 const Footer = () => {
-  return <h1>Footer</h1>;
+  return <footer className="footer">footer</footer>;
 };
 
 export default Footer;
--- components/Header.js
+++ components/Header.js
@@ -1,51 +1,56 @@
 import Image from 'next/image';
+import { useState } from 'react';
 import Logo from '../public/images/logos/logo-flat.png';
+import { HiMenuAlt4, HiX } from 'react-icons/hi';
 
 const Header = () => {
+  const [menuActive, setMenuActive] = useState(false);
+  // const toggleMenu = () => {
+  //   setMenuActive((prev) => !prev);
+  // };
+
   return (
-    <div className="header">
+    <header className="header">
       <nav>
+        {!menuActive && <HiMenuAlt4 className="menu-icon" onClick={() => setMenuActive(true)} />}
+        {menuActive && <HiX className="menu-icon" onClick={() => setMenuActive(false)} />}
         <div className="logo">
-          {/* <img src="" alt="" /> */}
-          <Image
-            src={Logo}
-            alt="My property asia | thailand"
-            // blurDataURL="data:..." automatically provided
-            // placeholder="blur" // Optional blur-up while loading
-          />
+          <Image src={Logo} alt="My property asia | thailand" />
         </div>
-        <div className="navbar">
-          <ul>
-            <li>
-              <a href="/">Buy</a>
-            </li>
-            <li>
-              <a href="/">Rent</a>
-            </li>
-            <li>
-              <a href="/">Condos</a>
-            </li>
-            <li>
-              <a href="/">Commercial</a>
-            </li>
-            <li>
-              <a href="/">Find Agent</a>
-            </li>
-            <li>
-              <a href="/">News</a>
-            </li>
-            <li>
-              <a href="/">Advertise</a>
-            </li>
-          </ul>
-          <div className="auth">
-            <div className="login">
-              <a href="/login">Login</a>
+        {menuActive && (
+          <div className="navbar">
+            <ul>
+              <li>
+                <a href="/">Buy</a>
+              </li>
+              <li>
+                <a href="/">Rent</a>
+              </li>
+              <li>
+                <a href="/">Condos</a>
+              </li>
+              <li>
+                <a href="/">Commercial</a>
+              </li>
+              <li>
+                <a href="/">Find Agent</a>
+              </li>
+              <li>
+                <a href="/">News</a>
+              </li>
+              <li>
+                <a href="/">Advertise</a>
+              </li>
+            </ul>
+            <div className="auth">
+              <div className="login">
+                <a href="/login">Login</a>
+              </div>
             </div>
           </div>
-        </div>
+        )}
       </nav>
-    </div>
+    </header>
   );
 };
 
