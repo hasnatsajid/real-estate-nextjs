fe3c1ac67f8de9d61f8b937a851e400fcf471c97
hasnatsajid965 <86589885+hasnatsajid@users.noreply.github.com>
2022-08-08 18:34:52 +0500

    property card done

--- components/Listing.js
+++ components/Listing.js
@@ -1,13 +1,24 @@
 import Image from 'next/image';
 import { BiBed, BiBath, BiCar } from 'react-icons/bi';
+import { FaUserCircle, FaHouseUser } from 'react-icons/fa';
 
-const Listing = ({ img }) => {
+const Listing = ({ img, Agent }) => {
   return (
     <article className="listing mt-3 mb-5">
       <div className="listing__container">
-        <div className="listing__agent"></div>
+        <div className="listing__agent">
+          <div className="listing__agent-name">
+            <span>
+              <FaHouseUser />
+            </span>
+            Johny mony
+          </div>
+          <div className="listing__agent-company">
+            <Image src={Agent} />
+          </div>
+        </div>
         <div className="listing__featured-img">
-          <Image src={img} width="550px" height="450px" />
+          <Image src={img} />
         </div>
         <div className="listing__details">
           <div className="listing__details-price">$325,000</div>
--- pages/buy.js
+++ pages/buy.js
@@ -6,6 +6,9 @@ import Listing from '../components/Listing';
 import house1 from '../public/houses/house1.jpg';
 import house2 from '../public/houses/house2.jpg';
 
+import Agent1 from '../public/realestates/newdoor.jpg';
+import Agent2 from '../public/realestates/penhomes.jpg';
+
 export default function Buy() {
   return (
     <div className="app">
@@ -21,8 +24,8 @@ export default function Buy() {
           <div className="listings__container">
             <div className="category-info">
               <h3 className="mt-4">158,427 Results of Property For Sale in Thailand</h3>
-              <Listing img={house1} />
-              <Listing img={house2} />
+              <Listing img={house1} Agent={Agent1} />
+              <Listing img={house2} Agent={Agent2} />
             </div>
           </div>
         </div>
