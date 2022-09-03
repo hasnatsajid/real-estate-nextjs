b874724b0d78909262336fa9676d57bc86fea482
hasnatsajid <hasnatsajid965@gmail.com>
2022-09-03 12:09:27 +0500

    added agent listing adding

--- components/NewProperty.js
+++ components/NewProperty.js
@@ -11,8 +11,28 @@ const NewProperty = () => {
             </div>
           );
         })}
+      </div>
 
-        <div className="property-details"></div>
+      <div className="detail-wrapper">
+        <div className="property-details">
+          <div className="title">
+            <h1>Property Details</h1>
+          </div>
+
+          <div className="sub-title">
+            <h3>What type of property do you have?</h3>
+          </div>
+
+          <div className="types">
+            <select name="types" id="types">
+              <option value="house" selected>
+                House
+              </option>
+              <option value="apartment">Apartment</option>
+              <option value="commercial">Commercial</option>
+            </select>
+          </div>
+        </div>
       </div>
     </div>
   );
